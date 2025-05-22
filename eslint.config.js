9736e4798e8a4ef832943bd2cf5e6788298cc6a2
Denys Rudyk <rudykdenys@gmail.com>
2025-05-23 00:04:35 +0300

    chore: add eslint plugins

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,20 @@
 // https://docs.expo.dev/guides/using-eslint/
-const { defineConfig } = require('eslint/config');
-const expoConfig = require('eslint-config-expo/flat');
+const { defineConfig } = require("eslint/config");
+const expoConfig = require("eslint-config-expo/flat");
+const expoPlugin = require("eslint-plugin-expo");
+const simpleImportSortConfig = require("eslint-plugin-simple-import-sort");
 
 module.exports = defineConfig([
   expoConfig,
   {
-    ignores: ['dist/*'],
+    ignores: ["dist/*"],
+    plugins: {
+      expo: expoPlugin,
+      "simple-import-sort": simpleImportSortConfig,
+    },
+    rules: {
+      "simple-import-sort/imports": "error",
+      "simple-import-sort/exports": "error",
+    },
   },
 ]);
